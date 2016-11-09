05be24d45b7be0ffcbb3899aaf0c6eb4966ab50a
Christian Elberfeld <elberfeld@web.de>
2016-11-09 20:14:52 +0100

    lokaler dns server

--- webserver/nginx/templates/letsencrypt.sh
+++ webserver/nginx/templates/letsencrypt.sh
@@ -6,5 +6,5 @@ if [ ! -e venv/bin/python ]; then ./venv.sh; fi
 cd /etc/ssl 
 PATH=/opt/simp_le/venv/bin:/usr/sbin:/usr/bin:/sbin:/bin 
 
-simp_le --email {{ letsencrypt_mail }} -f account_key.json -f key.pem -f fullchain.pem --tos_sha256 {{ letsencrypt_tos_sha256 }} {% for domain in webserver_domains %} -d {{ domain }}:/var/www/html {% endfor %} && systemctl reload nginx
+simp_le --email {{ letsencrypt_mail }} -f account_key.json -f key.pem -f fullchain.pem --tos_sha256 {{ letsencrypt_tos_sha256 }} -d void.ms:/var/www/html {% for domain in webserver_domains %} -d {{ domain }}.void.ms:/var/www/html {% endfor %} && systemctl reload nginx
 
