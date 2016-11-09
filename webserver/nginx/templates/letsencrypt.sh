6a5b8ee233b6fee22e5e64bf12d510ec16de7c71
Christian Elberfeld <elberfeld@web.de>
2016-11-09 11:47:45 +0100

    webserver basiseinrichtung und etherpad server

---
+++ webserver/nginx/templates/letsencrypt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /opt/simp_le/ 
+if [ ! -e venv/bin/python ]; then ./venv.sh; fi 
+
+cd /etc/ssl 
+PATH=/opt/simp_le/venv/bin:/usr/sbin:/usr/bin:/sbin:/bin 
+
+simp_le --email {{ letsencrypt_mail }} -f account_key.json -f key.pem -f fullchain.pem --tos_sha256 {{ letsencrypt_tos_sha256 }} {% for domain in webserver_domains %} -d {{ domain }}:/var/www/html {% endfor %} && systemctl reload nginx
+
