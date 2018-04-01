bd2b168b18a116a68ad304f963136ad9812c0b14
Christian Elberfeld <elberfeld@web.de>
2018-04-01 03:00:20 +0200

    librenms tuning

--- intserver/docker_librenms/templates/config.custom.php
+++ intserver/docker_librenms/templates/config.custom.php
@@ -5,4 +5,12 @@
 $config['nets'][] = '10.5.0.0/24';
 $config['nets'][] = '192.168.0.0/24';
 
+# Ignorierte Interfaces
+$config['bad_if_regexp'][] = '/^lo.*$/';
+$config['bad_if_regexp'][] = '/^br.*$/';
+$config['bad_if_regexp'][] = '/^pf.*$/';
+$config['bad_if_regexp'][] = '/^veth.*$/';
+$config['bad_if_regexp'][] = '/^bridge.*$/';
+$config['bad_if_regexp'][] = '/^docker.*$/';
+
 ?>
