9393556a22ef15234876051ecafade2cda9c2544
Christian Elberfeld <christian.elberfeld@adesso.de>
2018-09-11 16:40:36 +0200

    wordpress auf webserver1

--- roles_webserver/nginx/templates/letsencrypt.sh
+++ roles_webserver/nginx/templates/letsencrypt.sh
@@ -3,6 +3,3 @@
 {% for domain in webserver_domains %}
 certbot certonly --non-interactive --agree-tos --webroot -m {{ letsencrypt_mail }} -w /var/www/html/  -d {{ domain }}.void.ms
 {% endfor %}
-
-systemctl reload nginx
-docker restart mail_app_1
