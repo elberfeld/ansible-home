0d5b0b4e21b86ca76c5f55f78607e32a14a364ac
Christian Elberfeld <christian.elberfeld@adesso.de>
2019-07-26 13:31:53 +0200

    Rsync backup auf internes NAS

---
+++ roles_common/rsyncbackup/templates/rsyncbackup-job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "===[ START RSync Job: {{ job.key }} ]===" \
+&& \
+/usr/bin/rsync --password-file=/srv/rsyncbackup/{{ job.key }}/rsync_password --verbose --archive --delete \
+{% if job.value.includes is defined %} \
+  {% for include in job.value.includes %}
+  --include="{{ include }}" \
+  {% endfor %} \
+{% endif %} \
+{% if job.value.excludes is defined %} \
+  {% for exclude in job.value.excludes %}
+  --exclude="{{ exclude }}" \
+  {% endfor %} \
+{% endif %} \
+{{ job.value.options }} \
+{{ job.value.basedir }} \
+{{ job.value.target }} \
+&& \
+echo "===[ END RSync Job: {{ job.key }} ]===" \
+&& \
+date > "/srv/rsyncbackup/{{ job.key }}/lastbackup" \
+&& \
+/srv/alerta_heartbeat/send_service_heartbeat.sh {{ job.value.heartbeat_timeout }} rsync@{{ job.key }}
