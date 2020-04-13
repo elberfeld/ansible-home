2530d2aab93880004578668bcdaa3c8b89a39a3d
Christian Elberfeld <christian.elberfeld@adesso.de>
2020-04-14 00:45:35 +0200

    umbau auf traefik

--- roles_webserver/docker_nextcloud/templates/nextcloud-index-worker.sh
+++ roles_webserver/docker_nextcloud/templates/nextcloud-index-worker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/docker exec -t nextcloud_app_1 su www-data -s "/bin/sh" -c "php /var/www/html/occ fulltextsearch:live"
+/usr/bin/docker exec -t {{ servicename }}_app_1 su www-data -s "/bin/sh" -c "php /var/www/html/occ fulltextsearch:live"
--- roles_webserver/nginx/templates/letsencrypt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-{% for domain in webserver_domains %}
-certbot certonly --non-interactive --agree-tos --webroot -m {{ letsencrypt_mail }} -w /var/www/html/  -d {{ domain }}.void.ms
-{% endfor %}
