256b28605749bb3e6c0417e8c78a9afee6a058d9
Christian Elberfeld <elberfeld@web.de>
2022-12-18 00:32:17 +0100

    more timeframes

--- roles_webserver/docker_freqtrade/templates/download-data.sh
+++ roles_webserver/docker_freqtrade/templates/download-data.sh
@@ -2,4 +2,4 @@
 #!/bin/sh 
 # Download Data from Exchange for hyperopt and backtest
 
-docker-compose run app download-data -t "1m 5m 15m 1h" $*
+docker-compose run app download-data -t "1m 5m 15m 1h 1d" $*
