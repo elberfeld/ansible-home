804f490aed9e11fc5eecfbd7a2032ed24780ffa0
Christian Elberfeld <elberfeld@web.de>
2023-01-14 22:27:14 +0100

    fix parameter

--- roles_webserver/docker_freqtrade/templates/download-data.sh
+++ roles_webserver/docker_freqtrade/templates/download-data.sh
@@ -2,4 +2,4 @@
 #!/bin/sh 
 # Download Data from Exchange for hyperopt and backtest
 
-docker-compose run app download-data -t "1m 5m 15m 1h 1d" $*
+docker-compose run app download-data -t 1m 5m 15m 30m 1h 4h 1d 1w 2w $*
