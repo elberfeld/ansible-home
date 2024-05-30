949b01416cff5c1e182682b52e1f42205db42399
Christian Elberfeld <6413499+elberfeld@users.noreply.github.com>
2024-05-31 00:41:24 +0200

    upgrade nextcloud

--- roles_webserver/docker_nextcloud/templates/occ.sh
+++ roles_webserver/docker_nextcloud/templates/occ.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Wrapper zur ausführung des OCC Kommendos im Docker 
-docker-compose exec app su www-data -s "/bin/sh" -c "php /var/www/html/occ $1 $2 $3"
+docker-compose exec app su www-data -s "/bin/sh" -c "php /var/www/html/occ $1 $2 $3 $4 $5 $6 $7 $8 $9"
